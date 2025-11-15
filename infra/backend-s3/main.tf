f9ec28ba1b0833fcb10b4896bac3df1ae233d209
Gilbert Daniel <daniel.gilbert247@gmail.com>
2025-11-15 19:54:51 +0530

    Add GitHub Action to Provision Backend-S3 for Terraform State Manamge

---
+++ infra/backend-s3/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.8.4"  # Allow Terraform 1.8.4 and newer versions
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.49.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "aws-eks-terraform-github-action-tf-state"
+    region         = "us-east-1"
+    key            = "eks/terraform.tfstate"
+    dynamodb_table = "Lock-Files"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
